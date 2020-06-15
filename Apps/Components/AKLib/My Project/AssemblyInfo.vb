1dbd43856fe49bcdcb9d16e01ff1356918f91e4c
Ardha Gp <tito.ardha@windowslive.com>
2020-06-15 11:30:23 +0000

    +upgrade components Imported from Changeset 522

--- Apps/Components/AKLib/My Project/AssemblyInfo.vb
+++ Apps/Components/AKLib/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.19361.0135")>
-<Assembly: AssemblyFileVersion("1.0.19361.0135")>
+<Assembly: AssemblyVersion("1.0.20167.1127")>
+<Assembly: AssemblyFileVersion("1.0.20167.1127")>
