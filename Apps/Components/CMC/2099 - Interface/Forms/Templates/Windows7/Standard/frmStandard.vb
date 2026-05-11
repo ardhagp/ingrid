9254138fa3bbc5559d82204857a0b5f25b470faa
ZENISLEV\titoa <tito.ardha@windowslive.com>
2026-05-11 11:34:41 +0700

    bugbash 20260511.2

--- Apps/Components/CMC/2099 - Interface/Forms/Templates/Windows7/Standard/frmStandard.vb
+++ Apps/Components/CMC/2099 - Interface/Forms/Templates/Windows7/Standard/frmStandard.vb
@@ -15,6 +15,7 @@ Public Class frmStandard
         End Try
     End Sub
 
+    <SupportedOSPlatform("windows")>
     Private Sub frmStandard_FormClosed(sender As Object, e As system.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
         If Not SLFStatusLabel.IsDisposed Then
             SLFStatusLabel.Text = ""
@@ -26,6 +27,7 @@ Public Class frmStandard
         GC.Collect(GC.MaxGeneration)
     End Sub
 
+    <SupportedOSPlatform("windows")>
     Private Sub frmStandard_ResizeEnd(sender As Object, e As EventArgs) Handles Me.ResizeEnd
         SLFNamaForm.Width = pnl_.Width - 111
         SLFSubNamaForm.Width = pnl_.Width - 134
@@ -39,6 +41,7 @@ Public Class frmStandard
     ''' This Function still not working 
     ''' </summary>
     ''' <returns></returns>
+    <SupportedOSPlatform("windows")>
     Public Function CheckTextInputs() As Integer
         Dim varViolation As Integer = 0
         Dim varAllTxt As New List(Of System.Windows.Forms.Control)
@@ -58,6 +61,7 @@ Public Class frmStandard
     ''' <param name="Parent"></param>
     ''' <param name="CtrlType"></param>
     ''' <returns></returns>
+    <SupportedOSPlatform("windows")>
     Private Shared Function FindControlRecursive(list As List(Of System.Windows.Forms.Control), ByVal Parent As System.Windows.Forms.Control, ByVal CtrlType As System.Type) As List(Of System.Windows.Forms.Control)
         If Parent Is Nothing Then Return list
         If Parent.GetType Is CtrlType Then
@@ -71,6 +75,7 @@ Public Class frmStandard
         Return list
     End Function
 
+    <SupportedOSPlatform("windows")>
     Public Sub CheckAllControls()
         Dim varAllControls As system.Windows.Forms.Control = Me.GetNextControl(Me, True)
 
